c7175893f869768e8157a2bae32a8cfb6b0e5d02
Shoaib Ahmad <shoaib.ahmad974@gmail.com>
2024-06-03 23:41:43 +0500

    🎉 Initial Commit

---
+++ account/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView
+from rest_framework_simplejwt.views import TokenRefreshView
+from rest_framework_simplejwt.views import TokenVerifyView
+
+
+app_name = "accounts_urls"
+
+urlpatterns = [
+    # For Authentication
+    path("auth/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("token/verify/", TokenVerifyView.as_view(), name="token_verify"),
+]
